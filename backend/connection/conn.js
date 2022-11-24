092c3218d638caaebdf1886b6d02905505873f1f
Pallavi Arora <pallavi.aro17@gmail.com>
2022-11-24 09:40:44 +0530

    Project Final

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 
-mongoose.connect("mongodb+srv://pal:pal%40cluster@cluster0.ghqtym1.mongodb.net/passiton?retryWrites=true&w=majority")
+mongoose.connect("<serverconnectionlink>")
 .then(()=>console.log("db connected"));
